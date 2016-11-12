182fb7707c276d231b7e80513768b506fe873b52
Norman Kabir <nkabir@gmail.com>
2016-11-12 14:31:20 -0600

    updating to 1.13

--- grammar.h
+++ grammar.h
@@ -1,14 +1,13 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+/* A Bison parser, made by GNU Bison 3.0.4.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +15,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -33,34 +30,43 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Tokens.  */
+#ifndef YY_YY_GRAMMAR_H_INCLUDED
+# define YY_YY_GRAMMAR_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     COLON = 258,
-     SEMICOLON = 259,
-     COMMA = 260,
-     EQUALS = 261,
-     PLUSEQUALS = 262,
-     EQUALSPLUS = 263,
-     QEQUALS = 264,
-     QPLUSEQUALS = 265,
-     LITERAL = 266,
-     NAME = 267,
-     PATH = 268,
-     LEFTPAREN = 269,
-     RIGHTPAREN = 270,
-     PREFIX = 271,
-     WILDCARD = 272,
-     ASSIGN = 273,
-     WITH = 274,
-     BEGIN_ANNOTATE = 275,
-     END_ANNOTATE = 276,
-     SCRIPT = 277,
-     ALIAS = 278
-   };
+  enum yytokentype
+  {
+    COLON = 258,
+    SEMICOLON = 259,
+    COMMA = 260,
+    EQUALS = 261,
+    PLUSEQUALS = 262,
+    EQUALSPLUS = 263,
+    QEQUALS = 264,
+    QPLUSEQUALS = 265,
+    LITERAL = 266,
+    NAME = 267,
+    PATH = 268,
+    LEFTPAREN = 269,
+    RIGHTPAREN = 270,
+    PREFIX = 271,
+    WILDCARD = 272,
+    ASSIGN = 273,
+    WITH = 274,
+    BEGIN_ANNOTATE = 275,
+    END_ANNOTATE = 276,
+    SCRIPT = 277,
+    ALIAS = 278
+  };
 #endif
 /* Tokens.  */
 #define COLON 258
@@ -85,13 +91,13 @@
 #define SCRIPT 277
 #define ALIAS 278
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
-#line 64 "grammar.y"
-{ 
+
+union YYSTYPE
+{
+#line 64 "grammar.y" /* yacc.c:1909  */
+ 
    linked_list* list;
    char* string;
    package_t* package;
@@ -101,14 +107,18 @@ typedef union YYSTYPE
    annotation_t* annotation;
    script_t* script;
    alias_t* alias;
-}
-/* Line 1529 of yacc.c.  */
-#line 107 "grammar.h"
-	YYSTYPE;
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
+
+#line 112 "grammar.h" /* yacc.c:1909  */
+};
+
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE yylval;
 
+int yyparse (void);
+
+#endif /* !YY_YY_GRAMMAR_H_INCLUDED  */
